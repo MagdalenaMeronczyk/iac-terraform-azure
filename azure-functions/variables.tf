a8f6be67845c3472edd9f0d31fcc35df80c76064
Magdalena Meronczyk <105588606+MagdalenaMeronczyk@users.noreply.github.com>
2023-05-23 17:47:34 +0200

    Create variables.tf

---
+++ azure-functions/variables.tf
@@ -0,0 +1,30 @@
+variable "location_resource_group" {
+  type = string
+}
+variable "name_resource_group" {
+  type = string
+}
+variable "name_storage_account" {
+  type = string
+}
+variable "account_replication_type_storage_account" {
+  type = string
+}
+variable "account_tier_storage_account" {
+  type = string
+}
+variable "name_app_service_plan" {
+  type = string
+}
+variable "size_sku_app_service_plan" {
+  type = string
+}
+variable "tier_sku_app_service_plan" {
+  type = string
+}
+variable "name_linux_function_app" {
+  type = string
+}
+variable "name_function_app_function" {
+  type = string
+}
