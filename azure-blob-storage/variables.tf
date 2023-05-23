043f3945882fb9081e5b39b6329e379c3acb8ee3
Magdalena Meronczyk <105588606+MagdalenaMeronczyk@users.noreply.github.com>
2023-05-23 17:53:25 +0200

    Create variables.tf

---
+++ azure-blob-storage/variables.tf
@@ -0,0 +1,24 @@
+variable "location_resource_group" {
+  type = string
+}
+variable "name_resource_group" {
+  type = string
+}
+variable "name_storage_account" {
+  type = string
+}
+variable "account_replication_type_storage_account" {
+  type = string
+}
+variable "account_tier_storage_account" {
+  type = string
+}
+variable "name_azurerm_storage_container" {
+  type = string
+}
+variable "name_storage_blob" {
+  type = string
+}
+variable "type_storage_blob" {
+  type = string
+}
